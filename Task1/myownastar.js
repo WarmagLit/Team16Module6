be1550b091ed1eda20a855321b76f246ed2e8a36
WarmagLit <genzeartem0@gmail.com>
2021-03-17 19:01:09 +0700

    Добавлен алгоритм A* v0.1

--- Task1/myownastar.js
+++ Task1/myownastar.js
@@ -1,17 +1,20 @@
 
-
 // Start and end
-// Width and height of each cell of world
 var start = 0;
 var end = 0;
-var w = 0;
-var h = 0;
 
 // How many columns and rows?
-var cols = 50;
-var rows = 50;
+var size = 25;
+
+
+// Width and height of each cell of world
+var width = 500;
+var height = 500;
+var w = width / size;
+var h = height / size;
+
 // This will be the 2D array
-var world = new Array(cols);
+var world = new Array(size);
 
 var current = 0;
 // The road taken
@@ -22,50 +25,85 @@ var path = [];
 var openSet = [];
 var closedSet = []; // for storing the old nodes
 
-
-// guess of how far it is between two nodes
-function heuristic(node, otheNode) {
-	return dist(node.x, node.y, otheNode.x, otheNode.y);
-}
-
 var Node = function () {
-    /**
-    *constructor
-    *@param {x} the x position of the node
-    *@param {y} the y position of the node
-    */
-        function Node(x, y) {
+    
+    this.nearNodes = []; // an array to hold the near nodes from the current node
+    this.f = 0; //the main function of A* Algorithm
+    this.g = 0; // g(n) is the cost of the path from the start node to n
+    this.h = 0; //h(n) is a heuristic that estimates the cost of the cheapest path from n to the goal   
+    this.block = false // if the current node is act like a wall or block ? createBlock() make it randomly 
+    if (Math.random(1) < 0.2) {
+        this.block = true;
+    }
+     function Node(x, y) {
     
                 this.x = x;
                 this.y = y;
-                this.f = 0; //the main function of A* Algorithm
-                this.g = 0; // g(n) is the cost of the path from the start node to n
-                this.h = 0; //h(n) is a heuristic that estimates the cost of the cheapest path from n to the goal
-                this.nearNodes = []; // an array to hold the near nodes from the current node
+                
                 this.previousNode = null;
-                this.block = this.createBlock(); // if the current node is act like a wall or block ? createBlock() make it randomly 
+               
         }
+
+        this.show = function(col) {
+            fill(col);
+            if (this.block) {
+                fill(0);
+            }
+            rect(this.x * w, this.y * h, w, h);
+        }
+
+        this.addNearNodes = function(world) {
+
+            if (this.x < size - 1) {
+               this.nearNodes.push(world[this.x + 1][this.y]);
+            }
+         
+            if (this.x > 0) {
+                this.nearNodes.push(world[this.x - 1][this.y]);
+            }
+            if (this.y < size - 1) {
+                this.nearNodes.push(world[this.x][this.y + 1]);
+            }
+            if (this.y > 0) {
+                this.nearNodes.push(world[this.x][this.y - 1]);
+            }
+           
+        }
+       
 }
     
+// guess of how far it is between two nodes
+function heuristic(node, otheNode) {
+    var d =  abs(node.x - otheNode.x) + abs(node.y - otheNode.y);
+    return d;
+}
 
 function createTheWorld() {
 	var i = void 0;
 	var j = void 0;
 
 	// Making a 2D array
-	for (i = 0; i < cols; i++) {
-		world[i] = new Array(rows);
+	for (i = 0; i < size; i++) {
+		world[i] = new Array(size);
 	}
 
-	for (i = 0; i < cols; i++) {
-		for (j = 0; j < rows; j++) {
-			world[i][j] = new Node(i, j);
+	for (j = 0; j < size; j++) {
+		for (i = 0; i < size; i++) {
+            world[i][j] = new Node(i, j);
+            world[i][j].x = i;
+            world[i][j].y = j;
+		}
+    }
+    
+    for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+            world[i][j].addNearNodes(world);
 		}
 	}
 
 	// Start and end
 	start = world[0][0];
-	end = world[cols - 1][rows - 1];
+	end = world[size - 1][size - 1];
 	//to make sure that the start and the end is not a block
 	start.block = false;
 	end.block = false;
@@ -74,35 +112,111 @@ function createTheWorld() {
 }
 
 function min_f (set) {
-    min = set[0];
+    minimal = set[0];
     for (var i = 0; i < set.length; i++) {
-        if (min.f > set[i].f) min = set[i];
+        if (minimal.f > set[i].f) {
+            minimal = set[i];
+        }
+    }
+    return minimal;
+}
+
+function removeElement(array, node) {
+    for (var i = array.length - 1; i >= 0; i--) {
+        if (array[i] == node) {
+            array.splice(i, 1);
+        }
     }
 }
 
-var n = new Node;
+function setup() {
+    createCanvas(400, 400);
+    background(0);
 
-function astar () {
-    var closed = [];
-    var open = [];
+    for (var i = 0; i < size; i++) {
+		for (var j = 0; j < size; j++) {
+            world[i][j].show(color(255));
+		}
+	}
+  }
+  
+  
+  function draw() {
+
+
+    if (openSet.length > 0) {
+
+        var lowest = min_f(openSet);
+        
+
+        if (lowest == end) {
+            //finding the path
+            var temp = lowest;
+            path.push(temp);
+            while (temp.previousNode) {
+                path.push(temp.previousNode);
+                temp = temp.previousNode;
+            }
+            noLoop();
+            console.log("Winner!");
+        }
+        
+        removeElement(openSet, lowest);
+        closedSet.push(lowest);
+
+        var neighbours = lowest.nearNodes;
+
+        for (var i = 0; i < neighbours.length; i++) {
+            var neighbor = neighbours[i];
+            if (!closedSet.includes(neighbor) && !neighbor.block) {
+                var possG = lowest.g + 1;
+
+                if (openSet.includes(neighbor)) {
+                    if (possG < neighbor.g) {
+                        neighbor.g = possG;
+                    }
+                } else{
+                    neighbor.g = possG;
+                    openSet.push(neighbor);
+                }
+
+                neighbor.previousNode = lowest;
+                neighbor.h = heuristic(neighbor, end);   
+                neighbor.f = neighbor.g + neighbor.h;
+            
+
+            }
+   
+        }
+    }
+    else {
+        return 0;
+    }
+
+    background(0);
+ 
+    for (var i = 0; i < size; i++) {
+		for (var j = 0; j < size; j++) {
+			world[i][j].show(color(255));
+		}
+    }
     
-    open.push(n);
-
-    while (open.length > 0) {
-        currnet = min_f(open);
-        if (current == end) return "success";
-        open.pop();
-
+    for (var i = 0; i < closedSet.length; i++) {
+        closedSet[i].show(color(255,0,0));
     }
-    return "failure";
+    
 
-}
+    for (var i = 0; i < openSet.length; i++) {
+        openSet[i].show(color(0,255,0));
+    }
+ 
+    for (var i = 0; i < path.length; i++) {
+        path[i].show(color(0,0,255));
+    }
+  }
+
+  
 
 createTheWorld();
-
-for (var k = 0; k < cols;k++) {
-    for (var j = 0; j < cols;j++) {
-        console.log(world[k][j].x);
-    }
-}
-console.log(n.x = 1, n.y, n.f, astar());
+ 
+console.log(world[1][1].x);
