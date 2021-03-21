76b0fd624e0f9585032bf1867ad43049f521ceb8
WarmagLit <genzeartem0@gmail.com>
2021-03-21 17:49:03 +0700

    Добавлен алгоритм кластеризации v1.0

--- 3Cluster analysis/k-means.js
+++ 3Cluster analysis/k-means.js
@@ -4,23 +4,42 @@ var h = 10;
 var size = 600;
 
 let btnClear = document.getElementById('clr');
+let btnClust = document.getElementById('cluster');
+let clusterAmount = document.getElementById('num');
 
 var allPoints = [];
+var centerPoints = [];
+
 
 var Point = function () {
     this.x;
     this.y;
+    this.isCenter = false; 
+    this.sum = 0;
+    this.Centroid = null;
+    this.color = 0;
+    this.theChosenOne = false;
 
-    function Point(x, y) {
-
-        this.x = x;
-        this.y = y;
+    this.spawn = function() {
+        while(true) {
+            let index = Math.floor(random(allPoints.length));
+            if (!allPoints[index].theChosenOne) {
+                this.x = allPoints[index].x;
+                this.y = allPoints[index].y;
+                allPoints[index].theChosenOne = true;
+                break;
+            }
+        }
     }
        
     this.show = function(col) {
+        this.color = col;
         fill(col);
-        ellipse(this.x, this.y, 5, 5);
-
+        if (this.isCenter) {
+            ellipse(this.x, this.y, 30, 30);
+        } else {
+            ellipse(this.x, this.y, 15, 15);
+        }
     }
 }
 
@@ -29,16 +48,111 @@ function mouseClicked() {
         var p = new Point();
         p.x = Math.floor(mouseX);
         p.y = Math.floor(mouseY);
-        console.log(p.x,p.y);
         allPoints.push(p);
         p.show(color(0,0,255));
-    }
+    }    
+}
 
+function distance(p1, p2) {
+    var d = sqrt( abs(p1.x - p2.x)*abs(p1.x - p2.x) +  abs(p1.y - p2.y)*abs(p1.y - p2.y));
+    return d;
+}
+
+function balancePoint(centroid) {
+    let byX = 0;
+    let byY = 0;
+    let numOfPoints = 0;
+    for (let i = 0; i < allPoints.length; i++) {
+        if (allPoints[i].Centroid == centroid) {
+            byX += allPoints[i].x;
+            byY += allPoints[i].y;
+            numOfPoints++;
+        }
+    }
+    centroid.x = Math.floor(byX / numOfPoints);
+    centroid.y = Math.floor(byY / numOfPoints);
+}
+
+function chooseCentroids() {
+        for (let j = 0; j < allPoints.length; j++) {
+            let minDist = 10000;
     
+            for (let i = 0; i < centerPoints.length; i++) {
+                if(distance(allPoints[j],centerPoints[i]) < minDist) {
+                    allPoints[j].Centroid = centerPoints[i];
+                    minDist = distance(allPoints[j],centerPoints[i]);
+                }
+            }
+        }
+        //calculate the sum for all centroids
+        for (var k = 0; k < allPoints.length; k++) {
+            allPoints[k].Centroid.sum += distance(allPoints[k],allPoints[k].Centroid);
+        }
+
 }
 
 btnClear.onclick = function() {
-    console.log('eee');
+    allPoints.length = 0;
+    centerPoints.length = 0;
+    setup();
+}
+
+btnClust.onclick = function() {
+    centerPoints.length = 0;
+    setup();
+    for (let i = 0; i < allPoints.length; i++) {
+        allPoints[i].Centroid = null;
+        allPoints[i].theChosenOne = false;
+    }
+
+    var centers = clusterAmount.value;
+    if (allPoints.length < centers) {
+        alert("Not enough points.\nPlease, place the points on the field with the left mouse button.");
+        return;
+    }
+
+    for (let i = 0; i < centers; i++) {
+        var centroid = new Point();
+        centroid.spawn();
+        centroid.isCenter = true;
+        centerPoints.push(centroid);
+    }
+
+    chooseCentroids();
+    
+    let isChanged = true;
+    while(isChanged) {
+        isChanged = false;
+        for (let i = 0; i < centerPoints.length; i++) {
+            let byX = centerPoints[i].x;
+            let byY = centerPoints[i].y;
+            balancePoint(centerPoints[i]);
+            
+            if (byX != centerPoints[i].x || byY != centerPoints[i].y) {
+                isChanged = true;
+            }
+            
+        }
+
+        for (let j = 0; j < allPoints.length; j++) {
+            let minDist = 10000;
+     
+            for (let i = 0; i < centerPoints.length; i++) {
+                if(distance(allPoints[j],centerPoints[i]) < minDist) {
+                    allPoints[j].Centroid = centerPoints[i];
+                    minDist = distance(allPoints[j],centerPoints[i]);
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < centerPoints.length; i++) {
+        centerPoints[i].show(color(Math.floor(random(255)),Math.floor(random(255)),Math.floor(random(255))));
+    }
+    for (let j = 0; j < allPoints.length; j++) {
+        allPoints[j].show(allPoints[j].Centroid.color);
+    }
+
 }
 
 
@@ -47,7 +161,8 @@ function setup() {
     createCanvas(900, 900);
     background(255);
 }
-  
+    
+
   
 function draw() {
   
