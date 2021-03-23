1f691b218d302ab8e2b6491023226a68fd9ce724
LAPTOP-1UIQE60B\OperThirteen <thebitvill2@gmail.com>
2021-03-24 02:20:40 +0700

    Доработка муравьиного алгоритма. Не прикручен рандом при выборе след. точки. Пока что вывод результата в консоль (длина всего пути).

--- 5Ant colony optimization/ant_colony.js
+++ 5Ant colony optimization/ant_colony.js
@@ -5,18 +5,16 @@ let btnStart = document.getElementById('start');
 
 
 var allPoints = [];
-
+var ants = [];
 
 var Point = function () {
     this.x;
     this.y;
 
     this.pheromone;
-    this.jel;
 
-    this.bliz;
-
-    this.col = 0;
+    this.dist;
+    this.chance;
 
     function Point(x, y) {
 
@@ -34,17 +32,17 @@ var Point = function () {
             }
         }
 
-        fill(this.col);
+        fill(0);
         ellipse(this.x, this.y, 30, 30);
 
         fill(0);
         text(this.pheromone, this.x -8, this.y + 35);
 
         fill(255,0,255);
-        text(this.jel, this.x -8, this.y - 35);
+        text(this.tendency, this.x -8, this.y - 35);
 
         fill(0,0,255);
-        text(this.bliz, this.x -20, this.y - 50);
+        text(this.chance, this.x -20, this.y - 50);
     }
 
 }
@@ -54,19 +52,21 @@ var Ant = function () {
     this.x;
     this.y;
     this.visited = [];
+    this.dist = 0;
 
     this.show = function() {
         fill(255,0,0);
         ellipse(this.x, this.y, 15, 15);
     }
-}
 
-function rast(x,y){
-
-   return (Math.sqrt(Math.pow(allPoints[0].x - x,2)+Math.pow(allPoints[0].y - y,2)));
+    this.distance = function(x2,y2){
+        this.dist += (Math.sqrt(Math.pow(this.x - x2,2)+Math.pow(this.y - y2,2)));
+        return (Math.sqrt(Math.pow(this.x - x2,2)+Math.pow(this.y - y2,2)));
+    }
 }
 
 function mouseClicked() {
+
     if (mouseX > 0 && mouseX < 900 && mouseY > 0 && mouseY < 900) {
         var p = new Point();
 
@@ -74,18 +74,7 @@ function mouseClicked() {
         p.y = Math.floor(mouseY);
         p.pheromone = 0.2;
 
-        if(allPoints.length > 0){
-            var r = rast(p.x,p.y);
-            p.jel = 200/ r;
-        }
-
         allPoints.push(p);
-
-        if(allPoints.length == 1){
-            a = new Ant();
-                a.x = allPoints[0].x; 
-                a.y = allPoints[0].y;
-            }
     }
 }
 
@@ -95,27 +84,28 @@ function setup() {
     background(255);
 }
   
-
-var a;
-var check = true;
-
-btnStart.onclick = function(){
+function calculate(a) {
 
     var denominator = 0;
+    for(var i = 0; i < allPoints.length; i++){
 
-    if(check){
-        a.visited.length =  allPoints.length;
-        a.visited.fill(0)
-        check = false;
+        if(a.x != allPoints[i].x && a.y != allPoints[i].y){
+
+            var r = a.distance(allPoints[i].x,allPoints[i].y);
+            allPoints[i].tendency = 200/ r;
+            denominator += (allPoints[i].pheromone * allPoints[i].tendency);
+
+            allPoints[i].pheromone *= allPoints[i].pheromone;
+        }
     }
 
-    for(var i = 1; i < allPoints.length; i++){
-        denominator += (allPoints[i].pheromone * allPoints[i].jel);
+    for(var i = 0; i < allPoints.length; i++){
+        
+        allPoints[i].chance = allPoints[i].tendency / denominator;
     }
+}
 
-    for(var i = 1; i < allPoints.length; i++){
-        allPoints[i].bliz = allPoints[i].jel / denominator;
-    }
+function findNextNode(a) {
 
     var maxB = 0;
     var maxX;
@@ -124,9 +114,9 @@ btnStart.onclick = function(){
 
     for(var i = 0; i < allPoints.length; i++){
         
-        if(maxB < allPoints[i].bliz && a.visited[i] == 0){
+        if(maxB < allPoints[i].chance && a.visited[i] == 0){
 
-            maxB = allPoints[i].bliz;
+            maxB = allPoints[i].chance;
             maxX = allPoints[i].x;
             maxY = allPoints[i].y;
             ind = i;
@@ -137,23 +127,50 @@ btnStart.onclick = function(){
     a.x = maxX;
     a.y = maxY;
     a.visited[ind] = 1;
+    allPoints[ind].pheromone /= 0.2;
 
-    allPoints[ind].col = 255;
+}
+
+
+
+btnStart.onclick = function(){
+
+    for(var i = 0; i < 3; i++){
+        var a = new Ant();
+
+        a.x = allPoints[0].x; 
+        a.y = allPoints[0].y;
+        ants.push(a);
+    }
+
+    
+    for(var j = 0; j < 3; j++){
+        ants[j].visited.length =  allPoints.length;
+        ants[j].visited.fill(0);
+        ants[j].visited[0] = 1
+        
+        for(var i = 1; i < allPoints.length; i++){
+            calculate(ants[j]);
+            findNextNode(ants[j]);
+        }     
+    }
+
+    for(var j = 0; j < 3; j++){
+        console.log(ants[j].dist);
+    }
 }
   
 btnClear.onclick = function() {   
     allPoints = [];
+    ants = [];
     setup();
 }
 
 
-function draw(){
 
-    if(allPoints.length > 0){
-        background(255);
-       for(var i = 0; i < allPoints.length; i++){
-           allPoints[i].show();
-       }
-       a.show();
+function draw(){   
+    background(255);
+    for(var i = 0; i < allPoints.length; i++){
+        allPoints[i].show();
     }
 }
