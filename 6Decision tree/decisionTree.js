eb23b26bc8fb4e6d2fc59dbdd0946bc9b907e8ff
Eugene <nexp1e@stud.tsu.ru>
2021-04-09 19:23:14 +0700

    Рабочий алгоритм построения дерева на основе csv файла. Пока без визуализации

--- 6Decision tree/decisionTree.js
+++ 6Decision tree/decisionTree.js
@@ -1,122 +1,234 @@
 //parser config
 const papaConfig = {
     complete: function(results) {
-        console.log("Parsing complete!");
+        alert("Parsing complete!");
     }
 }
 
+const fileSubmit = document.getElementById("csvFile");
+const selectionTypes = [];
 
-//data table
-//добавить возможность работать с именованными параметрами
-const fileSubmitButton = document.getElementById("fileSubmitButton");
-let csvFile;
+let attrNames = [];
 let dataTable;
 let rootNode;
 
-//добавить проверки на формат и наличие файла
-function fileSubmittion() {
-    csvFile = document.getElementById("csvFile").files[0];
-    dataTable = Papa.parse(csvFile, papaConfig).data;
-    console.log("file submitted successfully");
-}
-fileSubmitButton.addEventListener("click", fileSubmittion);
+function defineSelectionTypes() {
+    selectionTypes.length = dataTable[0].length - 1;
 
-
-
-
-//tree element 
-
-class Node {
-    constructor(treeLevel, selectionType, inpurity, attribute, parent = -1, isLeaf = fasle, children = {}) {
-        this.treeLevel = treeLevel;
-        this.selectionType = selectionType;
-        this.impurity = impurity;
-        this.children = children;
-        this.isLeaf = isLeaf;
-        this.parent = parent;
-        this.attribute = attribute;
-    }
-
-}
-
-
-//Gini impurity for root
-function singleLowestImpurity(attributes) {
-    let impurities = {};
-    impurities.length = attributes.length;
-    //только для булевых параметров
-    for (let i = 0; i < attributes.length; i++) 
-    {
-        let trues = 0;
-        let falses = 0;
-        for (let j = 0; j < dataTable[attributes[i]].length; j++) {
-            if (dataTable[i][j] === true) {
-                trues++;
+    for (let i = 0; i < selectionTypes.length; i++) {
+        selectionTypes[i] = "numeric";
+        for (let j = 0; j < dataTable[i].length; j++) {
+            if (dataTable[j][i] != Number(dataTable[j][i])) {
+                selectionTypes[i] = "selection";
+                break;
             }
-            else {
-                falses++;
+        }
+    }
+}
+
+//добавить проверки на формат файла
+function fileSubmittion(evt) {
+    let csvFilePath = evt.target.files[0];
+    let reader = new FileReader();
+    reader.readAsText(csvFilePath);
+
+    reader.onload = function(event) {
+        let csvFile = event.target.result;
+
+        dataTable = Papa.parse(csvFile, papaConfig).data;
+        console.log("file submitted successfully");
+        console.log(dataTable);
+
+        if (document.getElementById("attrHeaderCheck").checked) {
+            attrNames = dataTable[0];
+            dataTable.splice(0, 1);
+        }
+        else {
+            for (let i = 0; i < dataTable[0].length; i++) {
+                attrNames[i] = "Parameter №" + (i + 1);
             }
         }
 
-        impurities[i] = 1 - (trues / (trues + falses))*(trues / (trues + falses))
-                          - (falses / (trues + falses))*(falses / (trues + falses));
+        defineSelectionTypes();
+    }
+}
+
+fileSubmit.addEventListener("change", fileSubmittion);
+
+
+
+class Question {
+    constructor(attrIndex, value) {
+        this.attrIndex = attrIndex;
+        this.value = value;
     }
 
-    let minInpurityIndex = 0;
-    let minImpurityValue = impurities[0];
-    for (let i = 1; i < impurities.length; i++) {
-        if (impurities[i] < minImpurityValue) {
-            minImpurityValue = impurities[i];
-            minInpurityIndex = i;
+    match(row) {
+        let val = row[this.attrIndex];
+        if (selectionTypes[this.attrIndex] == "numeric") {
+            return (val >= this.value);
+        }
+        else {
+            return (val == this.value);
         }
     }
 
-    return {minInpurityIndex, minImpurityValue};
+    print() {
+        let condition = "==";
+        if (selectionTypes[this.attrIndex] == "numeric") {
+            condition = ">=";
+        }
+
+        return `Is ${attrNames[this.attrIndex]} ${condition} ${this.value}?`;
+    }
 }
 
-function multipleLowestImpurity(parents) {
-    let impurities = {};
-    impurities.length = attributes.length;
-    //только для булевых параметров
-    for (let j = 0; j < dataTable.length; j++) {
-        
-        
-        let trues = 0;
-        let falses = 0;
-        for (let i = 0; i < attributes.length; i++) {
-
+class leafNode {
+    constructor(dataset) {
+        this.predictions = answCount(dataset, attrNames.length - 1);
+    }
+    print() {
+        for (let key in this.predictions) {
+            console.log(`${key}: ${this.predictions[key]}`);
         }
     }
-    //attribute check
-
-    //
-
-    //добавить check на снижение impurity
-
-    //
-
-    return {minInpurityIndex, minImpurityValue, selectionType};
 }
 
-//определения типа параметра
-function getSelectionType(paranIndex) {
-
-}
-
-//все родительские элементы 
-function getParents(firstParent) {
-
-}
-
-//пока просто наброски. Наверняка нерабочая хуета
-function buildTree() {
-    rootNode = new Node(0, singleLowestImpurity(dataTable)[1], singleLowestImpurity(dataTable)[0]);
-    for (let i = 0; i <= 1; i++) {
-        rootNode.children[i] = new Node(rootNode.treeLevel + 1, multipleLowestImpurity(getParents(rootNode))[2], multipleLowestImpurity(getParents(rootNode))[0], multipleLowestImpurity(getParents(rootNode))[1]);
+class decisionNode {
+    constructor(question, trueBranch, falseBranch) {
+        this.question = question;
+        this.trueBranch = trueBranch;
+        this.falseBranch = falseBranch;
     }
+}
 
-
-    //механизм построения дерева
+function uniqueAttrValuesParcing(dataset, attrIndex) {
+    let values = [];
     
-    //
+    for (let i = 0; i < dataset.length; i++) {
+        if (!values.includes(dataset[i][attrIndex])) {
+            values.push(dataset[i][attrIndex]);
+        }
+    }
+
+    return values;
+}
+
+function answCount(dataset, attrIndex) {
+    let answers = {};
+
+    for (let i = 0; i < dataset.length; i++) {
+        let label = dataset[i][attrIndex];
+        if (!(label in answers)) {
+            answers[label] = 0;
+        }
+        answers[label]++;
+    }
+
+    return answers;
+}
+
+function partition(dataset, question) {
+    let trueRows = [];
+    let falseRows = [];
+
+    for (let i = 0; i < dataset.length; i++) {
+        if (question.match(dataset[i])) {
+            trueRows.push(dataset[i]);
+        }
+        else {
+            falseRows.push(dataset[i]);
+        }
+    }
+
+    return [trueRows, falseRows];
+}
+
+function countGiniImp(dataset) {
+    let answers = answCount(dataset, attrNames.length - 1);
+    let impurity = 1;
+
+    for (let answer in answers) {
+        answProbability = answers[answer] / dataset.length;
+        impurity -= answProbability*answProbability;
+    }
+
+    return impurity;
+}
+
+function countInfoGain(currUncertainty, trueBranch, falseBranch) {
+    let proportion = trueBranch.length / (trueBranch.length + falseBranch.length);
+
+    return currUncertainty - countGiniImp(trueBranch) * proportion - countGiniImp(falseBranch) * (1 - proportion);
+}
+
+function split(dataset) {
+    let bestGain = 0;
+    let bestQuestion = null;
+    currUncertainty = countGiniImp(dataset);
+
+    for (let i = 0; i < attrNames.length - 1; i++) {
+        let values = answCount(dataset, i);
+
+        for (let value in values) {
+            let question = new Question(i, value);
+
+            let p = partition(dataset, question);
+            let trueRows = p[0];
+            let falseRows = p[1];
+
+            if (!trueRows.length || !falseRows.length) {
+                continue;
+            }
+
+            let gain = countInfoGain(currUncertainty, trueRows, falseRows);
+            if (gain > bestGain) {
+                bestGain = gain;
+                bestQuestion = question;
+            }
+        }
+    }
+
+    return [bestGain, bestQuestion];
+}
+
+function buildTree(dataset) {
+    console.log("working with dataset: " + dataset);
+
+    console.log(dataset);
+    let s = split(dataset);
+    let gain = s[0];
+    let question = s[1];
+    
+
+    if (gain <= 0 || question === null) {
+        return new leafNode(dataset);
+    }
+    console.log(question.print());
+
+    let p = partition(dataset, question);
+    let trueRows = p[0];
+    let falseRows = p[1];
+
+    let trueBranch = buildTree(trueRows);
+
+    let falseBranch = buildTree(falseRows);
+
+    return new decisionNode(question, trueBranch, falseBranch);
+}
+
+function printTree(node, spacing) {
+    if (node instanceof leafNode) {
+        console.log(spacing + "Predict: ")
+        node.print();
+        return;
+    }
+
+    console.log(spacing + node.question.print());
+
+    console.log(spacing + '--> True:');
+    printTree(node.trueBranch, spacing + "  ");
+
+    console.log(spacing + '--> False:');
+    printTree(node.falseBranch, spacing + "  ");
 }
